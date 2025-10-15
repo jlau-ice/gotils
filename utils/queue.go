c3a5d1b2981e910dc7af08ce8541271a71487a47
ganbin <jlau.ice@gmail.com>
2025-10-15 14:08:18 +0800

    feat: add queue tool

---
+++ utils/queue.go
@@ -0,0 +1,36 @@
+package utils
+
+type Queue[T any] struct {
+	items []T
+}
+
+func (q *Queue[T]) Enqueue(item T) {
+	q.items = append(q.items, item)
+}
+
+func (q *Queue[T]) Dequeue() (T, bool) {
+	if len(q.items) == 0 {
+		var zero T
+		return zero, false
+	}
+	item := q.items[0]
+	q.items = q.items[1:]
+	return item, true
+}
+
+func (q *Queue[T]) Peek() (T, bool) {
+	if len(q.items) == 0 {
+		var zero T
+		return zero, false
+	}
+	return q.items[0], true
+}
+
+func (q *Queue[T]) Len() int {
+	return len(q.items)
+}
+
+func main() {
+	q := Queue[int]{}
+	q.Enqueue(1)
+}
